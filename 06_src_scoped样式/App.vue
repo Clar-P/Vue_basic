e1b7fdaef73dccdb9d9d9279db8c3613e315e494
pujing <316910570@qq.com>
2023-04-22 19:36:43 +0800

    scoped样式

---
+++ 06_src_scoped样式/App.vue
@@ -0,0 +1,26 @@
+<template>
+  <div>
+    <h1  class="title">Nihao</h1>
+    <Student></Student>
+    <hr>
+    <School></School>
+  </div>
+</template>
+
+<script>
+    // 引入student组件
+import Student from './components/Student.vue'
+import School from './components/School.vue'
+
+export default {
+    name:'App',
+    components:{Student,School}
+}
+</script>
+
+<style>
+  .title{
+    background-color: red;
+  }
+</style>
+
